04f60e91a3a8de47e4f92c53d0dd841a739ab343
Arshad <ahmed.a0611@gmail.com>
2023-06-27 13:29:41 +0530

    error resolved

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 const API_KEY="7b974877233e4d03ac8189912b3418d5";
 const url="https://newsapi.org/v2/everything?q=";
 
-window.addEventListener('load',()=>fetchNews("Inida"));
+window.addEventListener("load", () => fetchNews("India"));
 
-async function fetchNews (query){
+function reload() {
+    window.location.reload();
+}
+
+async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
-    const data =  await res.json();
+    const data = await res.json();
     bindData(data.articles);
 }
 
-function bindData(articles){
-    const cardContainer=document.getElementById('card-container');
-    const newsCardTemplate=document.getElementById("template-news-card");
+function bindData(articles) {
+    const cardsContainer = document.getElementById("card-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
 
-    cardContainer.innerHTML="";
+    cardsContainer.innerHTML = "";
 
-    articles.forEach((article)=>{
-        if(!article.urlToImage) return;
-        const cardClone= newsCardTemplate.content.cloneNode(true);
-        fillDataInCard(cardClone,article);
-        cardContainer.appendChild(cardClone);
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
     });
 }
 
-function fillDataInCard(cardClone,article){
-    const newsImg = cardClone.querySelector('#news-img');
-    const newsTitle = cardClone.querySelector('#news-title');
-    // const newsSource= cardClone.querySelector('#news-source');
-    const newsDesc = cardClone.querySelector('#news-desc');
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
 
-    newsImg.src=article.urlToImage;
-    newsTitle.innerHTML=article.title;
-    newsDesc.innerHTML=article.description;
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleDateString( "en-US",{
+        timeZone:"Asia/Jakarta"
+    });
+
+    newsSource.innerHTML= `${article.source.name}• ${date}`
 
 }
